f3f11b7b91043c68bbeb46ff99c8d8671263a587
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2019-01-28 22:56:01 +0200

    dont bother to open it-nothing is there, just write me 4

--- 5/src/main/java/org/elsys/netprog/rest/ServerLogic.java
+++ 5/src/main/java/org/elsys/netprog/rest/ServerLogic.java
@@ -29,7 +29,7 @@ public class ServerLogic {
 	public Response startGame() throws URISyntaxException{
 		
 		return Response.created(new URI("/games")).build();
-		
+		//I just dont know what to do
 	}
 	
 	
